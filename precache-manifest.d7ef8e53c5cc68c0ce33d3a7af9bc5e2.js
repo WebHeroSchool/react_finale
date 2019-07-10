4c6a64dcddb5f666c508461204bc0240002403c7
Victor Zhuravlev <mocambe@mail.ru>
2019-07-10 13:16:51 +0300

    Updates

---
+++ precache-manifest.d7ef8e53c5cc68c0ce33d3a7af9bc5e2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c37183eda03bd066a1c60d67085897",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d81bb36d295ef285074f",
+    "url": "/static/css/main.9169dd4a.chunk.css"
+  },
+  {
+    "revision": "ee3ed2235b3b3a1c3cdd",
+    "url": "/static/js/2.c7680b8b.chunk.js"
+  },
+  {
+    "revision": "d81bb36d295ef285074f",
+    "url": "/static/js/main.6e8501c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fc63fe640905b4db164af92a5e4fbc05",
+    "url": "/static/media/button-add.fc63fe64.svg"
+  },
+  {
+    "revision": "320fefeabe01afbdde0954c792145100",
+    "url": "/static/media/button-delete.320fefea.svg"
+  },
+  {
+    "revision": "4e5f3dae5a09b7e97f5cee129829f8ad",
+    "url": "/static/media/checkbox-checked.4e5f3dae.svg"
+  },
+  {
+    "revision": "fb6a96fd421c1914605fe3bfc95a80eb",
+    "url": "/static/media/checkbox-empty.fb6a96fd.svg"
+  },
+  {
+    "revision": "bfcb6328fbf9d7112b5991301c49d0fa",
+    "url": "/static/media/email.bfcb6328.svg"
+  },
+  {
+    "revision": "a1d68ab9ed17a773046e97204989884d",
+    "url": "/static/media/empty-list.a1d68ab9.svg"
+  },
+  {
+    "revision": "584b642af754da2e11999cf7340c3d3e",
+    "url": "/static/media/facebook.584b642a.svg"
+  },
+  {
+    "revision": "0a1558bf5081fe0d139f0eb344d36182",
+    "url": "/static/media/fork.0a1558bf.svg"
+  },
+  {
+    "revision": "3feade3d4db28bbf7d006510b64309a4",
+    "url": "/static/media/github.3feade3d.svg"
+  },
+  {
+    "revision": "2ae91a719f8fce8c480a57678bf105b9",
+    "url": "/static/media/linkedin.2ae91a71.svg"
+  },
+  {
+    "revision": "ac2ad3d5440c98bcfca21b106c9ea65f",
+    "url": "/static/media/loader.ac2ad3d5.svg"
+  },
+  {
+    "revision": "b60513e9da5538afe196cefe9dd427e5",
+    "url": "/static/media/snafu.b60513e9.svg"
+  },
+  {
+    "revision": "a3b344a246a1de75f22227e0e72f5216",
+    "url": "/static/media/star.a3b344a2.svg"
+  },
+  {
+    "revision": "60ef2abb42876cec5600e3f0c90d5b50",
+    "url": "/static/media/telegram.60ef2abb.svg"
+  },
+  {
+    "revision": "22582ca41c65310b5651bd42e31e4e02",
+    "url": "/static/media/vk.22582ca4.svg"
+  }
+]);
